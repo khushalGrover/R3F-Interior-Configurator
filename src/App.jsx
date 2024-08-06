aa16bbcced8c86ad970eaf09ddbc5e53a5e776b5
khushalGrover <khushal15grover@gmail.com>
2024-08-06 10:30:09 +0530

    final working

--- src/App.jsx
+++ src/App.jsx
@@ -177,7 +177,7 @@ function App() {
 									<button
 										className="bg-green-600 inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-9 rounded-md px-3 mt-4"
 										onClick={ () => {
-											Handle2ButtonClicked(removeAfterSecondUnderscore(objectProduct.docs[index].type) ,objectProduct.docs[index]._id);
+											Handle2ButtonClicked(removeAfterSecondUnderscore(objectProduct.docs[index].type) ,objectProduct.docs[index].product_id._id);
 											// Handle2ButtonClicked(objectProduct.docs[index].type ,objectProduct.docs[index]._id);
 										}}
 									>
